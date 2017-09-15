3128649877a5447c5107b89578729493bb2697a0
Mark Vonk <markjhvonk@gmail.com>
2017-09-16 01:40:22 +0200

    Default compression changes

--- public/js/app.js
+++ public/js/app.js
@@ -60,32 +60,28 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 8);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ({
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
-/***/ 40:
+__webpack_require__(1);
+module.exports = __webpack_require__(2);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
-/***/ }),
-
-/***/ 8:
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__(9);
-module.exports = __webpack_require__(40);
-
-
-/***/ }),
-
-/***/ 9:
-/***/ (function(module, exports) {
-
-
-
 /***/ })
-
-/******/ });
+/******/ ]);
