1892fee0f909f680882de6bf4e075d2fac566522
Mark Vonk <markjhvonk@gmail.com>
2017-09-16 01:41:41 +0200

    Added edit routes

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,12 @@ Route::get('/', function () {
 
 Route::get('/admin', 'AdminController@index')->name('dashboard');
 
-Route::get('/admin/studios', 'StudiosController@index');
-Route::get('/admin/studios/create', 'StudiosController@create');
-Route::get('/admin/studios/{studio}', 'StudiosController@studio');
-Route::post('/admin/studios', 'StudiosController@store');
+Route::get('/admin/studios', 'StudiosController@index');                // display all studios
+Route::get('/admin/studios/{studio}', 'StudiosController@studio');      // display specific studio
+Route::get('/admin/studios/create', 'StudiosController@create');        // create new studio form
+Route::post('/admin/studios', 'StudiosController@store');               // submit new studio
+Route::get('/admin/studios/{studio}/edit', 'StudiosController@edit');   // edit specific studio
+Route::patch('/admin/studios/{studio}', 'StudiosController@update');     // update specific studio
 
 Route::get('/admin/users', 'UsersController@index');
 Route::get('/admin/users/register', 'UsersController@create');
@@ -23,10 +25,10 @@ Route::get('/admin/logout', 'SessionsController@destroy');
 /* structure - basic resource controller
 
 GET     admin/studios           display all studios
+GET     admin/studios/{id}      display specific studio
 GET     admin/studios/create    create new studio
 POST    admin/studios submit    new studio
 GET     admin/studios/{id}/edit edit specific studio
-GET     admin/studios/{id}      display specific studio
 PATCH   admin/studios/{id}      update specific studio
 DELETE  admin/studios/{id}      delete specific studio
 
