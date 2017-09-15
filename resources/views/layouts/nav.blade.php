3125ac54bbee6beac1b46724fb5a9b1245ec77d0
Mark Vonk <markjhvonk@gmail.com>
2017-09-16 01:41:21 +0200

    Populated links in nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,11 +2,17 @@
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="">Home</a></li>
-        <li><a href="">Studios</a></li>
+        <li><a href="/admin">Home</a></li>
+        <li><a href="/admin/studios">Studios</a></li>
         <li><a href="">Bookings</a></li>
-        <li><a href="">Users</a></li>
-        {{--  <li><a href="">{{ Auth::user()->name }}</a></li>  --}}
+        <li><a href="/admin/users">Users</a></li>
+        @if (Auth::check())
+        <li>
+          <a href="">
+            {{ Auth::user()->name }}
+          </a>
+        </li>
+        @endif
       </ul>
     </div>
 </nav>
