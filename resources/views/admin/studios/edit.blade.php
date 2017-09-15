9956c2279ef94ca21cecc1f1af35a135a4a6f24f
Mark Vonk <markjhvonk@gmail.com>
2017-09-16 01:05:18 +0200

    Made edit form to edit studios

---
+++ resources/views/admin/studios/edit.blade.php
@@ -0,0 +1,207 @@
+@extends ('layouts.master')
+
+@section ('content')
+    <div class="row">
+        <h1>Edit: {{$studio->name}}</h1>
+    </div>
+    <form method="POST" action="../../studios/{{$studio->id}}" class="col s12">
+        {{ method_field('PATCH') }}
+        {{ csrf_field() }}
+        <div class="fixed-action-btn">
+            <button class="btn-floating btn-large red" type="submit">
+                <i class="large material-icons">save</i>
+            </button>
+        </div>
+        <div class="row">
+            <h4>General info</h4>
+        </div>
+        <div class="row">
+         <table>
+             <tr>
+                 <td><b>Created:</b></td>
+                 <td>{{ $studio->created_at->toFormattedDateString() }}</td>
+             </tr>
+             <tr>
+                 <td><b>Last updated:</b></td>
+                 <td>{{ $studio->updated_at->toFormattedDateString() }}</td>
+             </tr>
+         </table>
+             
+        </div>
+        <div class="row">
+            <div class="input-field col m5 s12">
+                <input name="name" id="name" type="text" class="validate" value="{{$studio->name}}">
+                <label for="name">Name</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col m8 s12">
+                <textarea name="info" id="info" class="materialize-textarea">{{$studio->info}}</textarea>
+                <label for="info">Info</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col m8 s12">
+                <textarea name="specs" id="specs" class="materialize-textarea">{{$studio->specs}}</textarea>
+                <label for="specs">Specs (seperated by ,)</label>
+            </div>
+        </div>
+        <div class="row">
+            <h4>Photo's</h4>
+        </div>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="../../../media/sample-1.jpg">
+                        {{--  <span class="card-title">Card Title</span>  --}}
+                    </div>   
+                    <div class="file-field input-field col s12">
+                        <div class="btn">
+                            <span>File</span>
+                            <input name="coverPhoto" id="coverPhoto" type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="New cover photo">
+                        </div>
+                    </div>
+                        
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image"><img src="../../../media/sample-1.jpg"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image"><img src="../../../media/sample-1.jpg"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image"><img src="../../../media/sample-1.jpg"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image"><img src="../../../media/sample-1.jpg"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image"><img src="../../../media/sample-1.jpg"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="file-field input-field col s6">
+                        <div class="btn">
+                            <span>Files</span>
+                            <input name="photos" id="photos" type="file" multiple>
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="Add more photos">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h4>Studio location:</h4>
+        </div>
+        <div class="row">
+            <div class="col s12 m6">
+            <iframe
+                width="100%" height="450"
+                frameborder="0" style="border:0"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDvLi1PKDdoUCpCAiixd4qOjzeOxO-GfTQ&q={{$studio->location}}"
+                allowfullscreen
+                ></iframe>
+            </div>
+            <div class="col s12 m6">
+                <div class="input-field col m8 s12">
+                    <input name="location" id="location" type="text" class="validate" value="{{$studio->location}}">
+                    <label for="location">Location</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h4>Assistance availability:</h4>
+        </div>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="switch">
+                    <label>
+                        No
+                        <input name="assistance" 
+                            type="checkbox"
+                            @if ($studio->assistance)
+                            checked="checked"
+                            @endif
+                            value="1">
+                        <span class="lever"></span>
+                        Yes
+                    </label>
+                    <br/><br/>
+                </div>
+            </div>
+        </div>
+    </form>
+    
+    <div class="row">
+        <div class="divider"></div>
+        <h4>Stuff that needs to be editable in the future:</h4>
+    </div>
+    <div class="row">
+        <h5>Equimpent kits</h5>
+        <ul>
+            @foreach ($studio->equipmentKits as $equipment)
+            <li>
+                <span>{{ $equipment->name }}</span>
+                <span>{{ $equipment->equipment }}</span>                  
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="row">
+        <h5>Colorama</h5>
+        <div class="colorama">
+            <ul>
+                @foreach ($studio->colorama as $colorama)
+                <li class="valign-wrapper center-align" style="background-color: #{{ $colorama->color}};">
+                    <span>{{ $colorama->color }}</span>
+                    <span>{{ $colorama->name }}</span>                  
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <h5>Lunch kits</h5>
+        <ul>
+            @foreach ($studio->lunchKits as $lunch)
+            <li>
+                <span>{{ $lunch->name }}</span>
+                <span>{{ $lunch->content }}</span>                  
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="row">
+        <h5>Minibar content</h5>
+        <ul>
+            @foreach ($studio->minibarContents as $minibar)
+            <li>
+                <span>{{ $minibar->name }}</span>              
+            </li>
+            @endforeach
+        </ul>
+    </div>
+@endsection
+
+@section ('footer')
+    <script src=""></script>
+@endsection
