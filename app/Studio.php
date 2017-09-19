60ba958364e670a161e54586942fb77971861a8e
Mark Vonk <markjhvonk@gmail.com>
2017-09-19 20:20:18 +0200

    preserved old functions for later use

--- app/Studio.php
+++ app/Studio.php
@@ -5,16 +5,21 @@ namespace App;
 
 class Studio extends Model
 {
-    public function colorama(){
-        return $this->belongsToMany(Colorama::class);
-    }
-    public function equipmentKits(){
-        return $this->belongsToMany(Equipment_kit::class);
-    }
-    public function lunchKits(){
-        return $this->belongsToMany(Lunch_kit::class);
-    }
-    public function minibarContents(){
-        return $this->belongsToMany(Minibar_content::class);
-    }
+
+
+
+    // Old relation stuff
+    // public function colorama(){
+    //     return $this->belongsToMany(Colorama::class);
+    // }
+    // public function equipmentKits(){
+    //     return $this->belongsToMany(Equipment_kit::class);
+    // }
+    // public function lunchKits(){
+    //     return $this->belongsToMany(Lunch_kit::class);
+    // }
+    // public function minibarContents(){
+    //     return $this->belongsToMany(Minibar_content::class);
+    // }
+    
 }
