a3a6a112ff846a467aba5c451f28d273efd2a786
Mark Vonk <markjhvonk@gmail.com>
2017-09-24 21:36:34 +0200

    Made relation to category

--- app/Equipment.php
+++ app/Equipment.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace App;
+use Illuminate\Database\Eloquent\Model;
 
 
 class Equipment extends Model
 {
-    //    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
