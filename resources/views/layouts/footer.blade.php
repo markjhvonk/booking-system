b81055a4a79c3c4770c0aede212dece06b8d421a
Mark Vonk <markjhvonk@gmail.com>
2017-09-24 21:38:16 +0200

    Adjusted footer so other code can be loaded in

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -11,3 +11,4 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="/js/app.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+@yield('footer')
