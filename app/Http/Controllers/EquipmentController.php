d2f5c5240f0dd726734f7f3db75d527eef88fad9
Mark Vonk <markjhvonk@gmail.com>
2017-09-24 21:37:11 +0200

    Made create and store functions for equipment

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -13,4 +13,28 @@ class EquipmentController extends Controller
         
         return view('admin.equipment.index', compact('equipment'));
     }
+
+    public function create()
+    {
+        // $equipment = Equipment::latest()->get();
+        
+        return view('admin.equipment.create');
+    }
+
+    public function store()
+    {
+        $this->validate(request(), [
+            'name'          => 'required|unique:equipment,name',
+            'description'   => '',
+            'data'          => '',
+            'price'         => ''
+            
+        ]);
+
+        // gather data and add it to the database
+        equipment::create(request(['name', 'description', 'data', 'price']));
+
+        //then redirect to the home page
+        return redirect('/admin/equipment');
+    }
 }
