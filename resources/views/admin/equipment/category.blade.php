2b60df27a11e0f57791e7cd5adc43440f457208b
Mark Vonk <markjhvonk@gmail.com>
2017-09-27 00:51:36 +0200

    Created category view for equipment pages

---
+++ resources/views/admin/equipment/category.blade.php
@@ -0,0 +1,72 @@
+@extends ('layouts.master')
+
+@section ('content')
+    <div class="row">
+        <h1>Equipment</h1>
+    </div>
+    <div class="row">
+        <div class="col s12 m12">
+            <a class="waves-effect waves-light btn" href="{{ url('admin/equipment/create') }}">new equipment</a>
+            <a class="waves-effect waves-light btn" href="{{ url('admin/equipment/create-category') }}">new category</a>
+        </div>
+    </div>
+    {{--  Category test  --}}
+    <div class="row">
+        <ul>
+            @foreach ($categories as $category)
+                <li>{{ $category->name }}</li>
+            @endforeach
+        </ul>
+    </div>
+    {{--  Tab test  --}}
+    Current Category data: {{ $current_category }}
+    <div class="row">
+        <div class="col s12">
+            <ul class="tabs">
+            @foreach ($categories as $category)
+                <li class="tab col s3">
+                    @if($category->id == $current_category->id)
+                    <a class="active" target="_self" href="{{ url('admin/equipment',$category->id) }}">{{ $category->name }}</a>
+                    @else
+                    <a target="_self" href="{{ url('admin/equipment',$category->id) }}">{{ $category->name }}</a>
+                    @endif
+                </li>
+                {{--  {{ str_replace(' ', '_', $category->name) }}  --}}
+            @endforeach
+            </ul>
+        </div>
+    </div>
+    {{--  Working code  --}}
+    <div class="row">
+        
+        <table class="striped">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Data</th>
+                    <th>Price</th>
+                    <th>Category</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            @foreach ($equipment as $equipment)
+                <tr>
+                    <td>{{ $equipment->name }}</td>
+                    <td>{{ $equipment->description }}</td>
+                    <td>{{ $equipment->data }}</td>
+                    <td>{{ $equipment->price }}</td>
+                    <td>{{ $equipment->category->name }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+    </div>
+
+@endsection
+
+@section ('footer')
+    <script src=""></script>
+@endsection
