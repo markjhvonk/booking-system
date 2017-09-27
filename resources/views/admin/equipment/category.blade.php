89b07ba5f46ef16c91e8d9a03b61102e47a213ab
Mark Vonk <markjhvonk@gmail.com>
2017-09-27 23:56:50 +0200

    Added code to display packages that belong to selected category

--- resources/views/admin/equipment/category.blade.php
+++ resources/views/admin/equipment/category.blade.php
@@ -27,41 +27,63 @@
     </div>
     <div class="row">
         <div class="col s12">
-            <table class="striped">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Data</th>
-                        <th>Price</th>
-                        <th>Category</th>
-                        <th></th>
-                    </tr>
-                </thead>
+            @foreach ($packages as $package)
+            <div class="col s6 m4">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">{{ $package->name }}</span>
+                        <p>{{ $package->description }}</p>
+                        <div class="divider"></div>
+                        <ul>
+                            @foreach ($package->equipment as $packageEquipment)
+                            <li>{{ $packageEquipment->name}}</li>
+                            @endforeach
+                        </ul>
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="col s12">
+            <div class="col s12">
+                <table class="striped">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Data</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th></th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                @foreach ($equipment as $equipment)
-                    <tr>
-                        <td>{{ $equipment->name }}</td>
-                        <td>{{ $equipment->description }}</td>
-                        <td>{{ $equipment->data }}</td>
-                        <td>{{ $equipment->price }}</td>
-                        <td>{{ $equipment->category->name }}</td>
-                        <td class="right-align">
-                            <a class="blue-text" href="{{ url('admin/equipment',$category->id) }}/edit"><i class="material-icons">edit</i></a>
-                            <form method="POST" action="{{ url('admin/equipment',$equipment->id) }}" style="display: inline-block;">
-                                {{ method_field('DELETE') }}
-                                {{ csrf_field() }}
-                                <button class="red-text clear-btn" type="submit"><i class="material-icons">delete</i></button>
-                            </form>
-                            <a class="green-text" href=""><i class="material-icons">visibility</i></a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    <tbody>
+                    @foreach ($equipment as $equipment)
+                        <tr>
+                            <td>{{ $equipment->name }}</td>
+                            <td>{{ $equipment->description }}</td>
+                            <td>{{ $equipment->data }}</td>
+                            <td>{{ $equipment->price }}</td>
+                            <td>{{ $equipment->category->name }}</td>
+                            <td class="right-align">
+                                <a class="blue-text" href="{{ url('admin/equipment',$category->id) }}/edit"><i class="material-icons">edit</i></a>
+                                <form method="POST" action="{{ url('admin/equipment',$equipment->id) }}" style="display: inline-block;">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <button class="red-text clear-btn" type="submit"><i class="material-icons">delete</i></button>
+                                </form>
+                                <a class="green-text" href=""><i class="material-icons">visibility</i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
+    
 
 @endsection
 
