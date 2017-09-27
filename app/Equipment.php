ad6f26730df3a111701421160923a76d5ef2cca1
Mark Vonk <markjhvonk@gmail.com>
2017-09-27 16:26:34 +0200

    Added fillables

--- app/Equipment.php
+++ app/Equipment.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipment extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+     protected $fillable = [
+        'name', 'description', 'data', 'price', 'category_id'
+    ];
+    
     public function category()
     {
         return $this->belongsTo(Category::class);
