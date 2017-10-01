743e26f01d4fcc6fc44cb271d575a93e174002e4
Mark Vonk <markjhvonk@gmail.com>
2017-10-01 16:45:31 +0200

    Added mass assignables

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+     protected $fillable = [
+        'name', 'description'
+    ];
 }
