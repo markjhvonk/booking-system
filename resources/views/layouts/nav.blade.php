401ae3c3e522a3f91ba227232df90b35e648b1d4
Mark Vonk <markjhvonk@gmail.com>
2017-10-01 16:50:18 +0200

    Added notation to menu auth check

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,24 +1,22 @@
 <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="{{ url('/admin') }}">Home</a></li>
-        <li><a href="{{ url('/admin/studios') }}">Studios</a></li>
-        <li><a href="{{ url('/admin/equipment') }}">Equipment</a></li>
-        <li><a href="">Bookings</a></li>
-        <li><a href="{{ url('/admin/users') }}">Users</a></li>
-        
-        <li>
-          @if (Auth::check())
-          <a href="{{ url('admin/logout') }}">
+  <div class="nav-wrapper">
+    <a href="#" class="brand-logo">Logo</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="{{ url('/admin') }}">Home</a></li>
+      <li><a href="{{ url('/admin/studios') }}">Studios</a></li>
+      <li><a href="{{ url('/admin/equipment') }}">Equipment</a></li>
+      <li><a href="">Bookings</a></li>
+      <li><a href="{{ url('/admin/users') }}">Users</a></li>
+      <li>
+      {{--  Check if user is logged in or not --}}
+        @if (Auth::check())
+        <a href="{{ url('admin/logout') }}">
             {{ Auth::user()->name }}
-          </a>
-          @else
-          <a href="{{ url('admin/login') }}">
+          </a> @else
+        <a href="{{ url('admin/login') }}">
             Login
-          </a>
-          @endif
-        </li>
-      </ul>
-    </div>
+          </a> @endif
+      </li>
+    </ul>
+  </div>
 </nav>
