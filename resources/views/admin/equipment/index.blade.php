852451888300343172e39c9cb7e9a19131276bee
Mark Vonk <markjhvonk@gmail.com>
2017-10-01 16:49:29 +0200

    Restructured layout + cleaned excessive/unused code

--- resources/views/admin/equipment/index.blade.php
+++ resources/views/admin/equipment/index.blade.php
@@ -2,43 +2,48 @@
 
 @section ('content')
     <div class="row">
-        <h1>Equipment</h1>
+        <h1>Categories</h1>
     </div>
-    <div class="row">
-        <div class="col s12 m12">
-            <a class="waves-effect waves-light btn" href="{{ url('admin/equipment/create') }}">new equipment</a>
-            <a class="waves-effect waves-light btn" href="{{ url('admin/equipment/category/create') }}">new category</a>
-            <a class="waves-effect waves-light btn" href="{{ url('admin/equipment/packages/create') }}">new kit</a>
-        </div>
-    </div>
-
     
-    {{--  Category test  --}}
     <div class="row">
-        <ul>
-            @foreach ($categories as $category)
-                <li>{{ $category->name }}</li>
-            @endforeach
-        </ul>
-    </div>
-
-
-    {{--  Tab test  --}}
-    <div class="row">
-        <div class="col s12">
-            <ul class="tabs">
-            @foreach ($categories as $category)
-                <li class="tab col s3">
-                    <a target="_self" href="{{ url('admin/equipment',$category->id) }}">{{ $category->name }}</a>
-                </li>
-                {{--  {{ str_replace(' ', '_', $category->name) }}  --}}
-            @endforeach
-            </ul>
+        @foreach ($categories as $category)
+        <div class="col s12 m4">
+            <a href="{{ url('admin/equipment',$category->id) }}">
+                <div class="card blue-grey hoverable">
+                    <div class="card-content white-text">
+                        <span class="card-title">{{ $category->name }}</span>
+                        <p>{{ $category->description }}</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        @endforeach
+        <div class="col s12 m4">
+            <div class="card blue-grey darken-1 hoverable">
+                <div class="card-content white-text">
+                    <span class="card-title">New category</span>
+                    <form method="POST" action="{{ url('admin/equipment/category') }}" class="col s12">
+                        {{ csrf_field() }}
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input name="name" id="name" type="text" class="validate">
+                                <label for="name">Name</label>
+                            </div>
+                            <div class="input-field col s12">
+                                <input name="description" id="description" type="text" class="validate">
+                                <label for="description">Description</label>
+                            </div>
+                        </div>
+                        <button class="btn-floating halfway-fab waves-effect waves-light"><i class="material-icons">send</i></button>
+                    </form>
+                    <div style="clear: both;"></div>
+                </div>
+            </div>
         </div>
     </div>
 
     {{--  Testing packages  --}}
-    <div class="row">
+    {{--  <div class="row">
         @foreach ($packages as $package)
         <div class="col s6 m4">
             <div class="card blue-grey darken-1">
@@ -56,10 +61,10 @@
             </div>
         </div>
         @endforeach    
-    </div>
+    </div>  --}}
 
     {{--  Working code  --}}
-    <div class="row">
+    {{--  <div class="row">
         
         <table class="striped">
             <thead>
@@ -85,7 +90,7 @@
             </tbody>
         </table>
 
-    </div>
+    </div>  --}}
 
 @endsection
 
