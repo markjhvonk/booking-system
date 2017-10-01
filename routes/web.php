760ff158733fe8f6dc55e674918db245b2204466
Mark Vonk <markjhvonk@gmail.com>
2017-10-01 21:30:22 +0200

    Added optional vars in some routes for better navigation and data display

--- routes/web.php
+++ routes/web.php
@@ -18,28 +18,28 @@ Route::patch('/admin/studios/{studio}', 'StudiosController@update');    // updat
 Route::delete('/admin/studios/{studio}', 'StudiosController@delete');   // delete specific studio
 
 // Equipment related routing
-Route::get('/admin/equipment', 'EquipmentController@index');                            // dashboard
-Route::get('/admin/equipment/create', 'EquipmentController@create');                    // create equipment item
-Route::post('/admin/equipment', 'EquipmentController@store');                           // submit equipment item
-Route::get('/admin/equipment/{equipment}/edit', 'EquipmentController@edit');            // edit equipment item
-Route::patch('/admin/equipment/{equipment}', 'EquipmentController@update');             // update equipment item
-Route::delete('/admin/equipment/{equipment}', 'EquipmentController@delete');            // delete equipment item
-Route::get('/admin/equipment/{current_category}', 'EquipmentController@category');      // display all equipment items from selected category
+Route::get('/admin/equipment', 'EquipmentController@index');                                // dashboard
+Route::get('/admin/equipment/create/{current_category?}', 'EquipmentController@create');    // create equipment item
+Route::post('/admin/equipment', 'EquipmentController@store');                               // submit equipment item
+Route::get('/admin/equipment/{equipment}/edit', 'EquipmentController@edit');                // edit   equipment item
+Route::patch('/admin/equipment/{equipment}', 'EquipmentController@update');                 // update equipment item
+Route::delete('/admin/equipment/{equipment}', 'EquipmentController@delete');                // delete equipment item
+Route::get('/admin/equipment/{current_category}', 'EquipmentController@category')->name('equipmentCategory');// display all equipment items from selected category
 
 // Category related routing
-Route::get('/admin/equipment/category/create', 'CategoriesController@create');            // create category
-Route::post('/admin/equipment/category', 'CategoriesController@store');                   // submit category
-Route::get('/admin/equipment/category/{category}/edit', 'CategoriesController@edit');     // edit category
-Route::patch('/admin/equipment/category/{category}', 'CategoriesController@update');      // update category
-Route::delete('/admin/equipment/category/{category}', 'CategoriesController@delete');     // delete category
+Route::get('/admin/equipment/category/create', 'CategoriesController@create');          // create category
+Route::post('/admin/equipment/category', 'CategoriesController@store');                 // submit category
+Route::get('/admin/equipment/category/{category}/edit', 'CategoriesController@edit');   // edit category
+Route::patch('/admin/equipment/category/{category}', 'CategoriesController@update');    // update category
+Route::delete('/admin/equipment/category/{category}', 'CategoriesController@delete');   // delete category
 
 // Packages related routing
-Route::get('/admin/equipment/packages/create', 'PackagesController@create');            // create package
-Route::post('/admin/equipment/packages', 'PackagesController@store');                   // submit package
-Route::get('/admin/equipment/packages/{package}', 'PackagesController@package');        // display package
-Route::get('/admin/equipment/packages/{package}/edit', 'PackagesController@edit');      // edit package
-Route::patch('/admin/equipment/packages/{package}', 'PackagesController@update');       // update package
-Route::delete('/admin/equipment/packages/{package}', 'PackagesController@delete');      // delete package
+Route::get('/admin/equipment/package/create/{current_category?}', 'PackagesController@create');    // create package
+Route::post('/admin/equipment/package/{current_category?}', 'PackagesController@store');           // submit package
+Route::get('/admin/equipment/package/{package}', 'PackagesController@package');                    // display package
+Route::get('/admin/equipment/package/{package}/edit', 'PackagesController@edit');                  // edit package
+Route::patch('/admin/equipment/package/{package}', 'PackagesController@update');                   // update package
+Route::delete('/admin/equipment/package/{package}', 'PackagesController@delete');                  // delete package
 
 // User related routing
 Route::get('/admin/users', 'UsersController@index');            // display all users
