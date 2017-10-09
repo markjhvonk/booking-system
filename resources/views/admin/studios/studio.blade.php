4a20c4062acd349c9ec0725b1ebdce837a247bfe
Mark Vonk <markjhvonk@gmail.com>
2017-10-09 19:08:22 +0200

    Removed old relation stuff, added buttons for functions

--- resources/views/admin/studios/studio.blade.php
+++ resources/views/admin/studios/studio.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.master')
 
 @section ('content')
-     <div class="fixed-action-btn">
+    <div class="fixed-action-btn">
         <a class="btn-floating btn-large red" href="">
             <i class="large material-icons">mode_edit</i>
         </a>
@@ -20,55 +20,23 @@
         <h1>{{$studio->name}}</h1>
     </div>
     <div class="row">
-        <p>{{$studio->info}}</p>
-    </div>
-    
-    {{-- old relation stuff --}}
-    {{--  <div class="row">
-        <h3>Equimpent kits</h3>
-        <ul>
-            @foreach ($studio->equipmentKits as $equipment)
-            <li>
-                <span>{{ $equipment->name }}</span>
-                <span>{{ $equipment->equipment }}</span>                  
-            </li>
-            @endforeach
-        </ul>
-    </div>
-    <div class="row">
-        <h3>Colorama</h3>
-        <div class="colorama">
-            <ul>
-                @foreach ($studio->colorama as $colorama)
-                <li class="valign-wrapper center-align" style="background-color: #{{ $colorama->color}};">
-                    <span>{{ $colorama->color }}</span>
-                    <span>{{ $colorama->name }}</span>                  
-                </li>
-                @endforeach
-            </ul>
+        <div class="col s12 red-text">
+            <a href="{{ url('admin/studios') }}" class="breadcrumb grey-text lighten-1">Studios</a>
+            <a href="#!" class="breadcrumb red-text">{{ $studio->name }}</a>
         </div>
     </div>
     <div class="row">
-        <h3>Lunch kits</h3>
-        <ul>
-            @foreach ($studio->lunchKits as $lunch)
-            <li>
-                <span>{{ $lunch->name }}</span>
-                <span>{{ $lunch->content }}</span>                  
-            </li>
-            @endforeach
-        </ul>
+        <a class="waves-effect waves-light btn yellow darken-1" href="{{ url('admin/studios',$studio->id) }}/edit">edit</a>
+        <a class="waves-effect waves-light btn yellow darken-1" href="{{ url('admin/studios',$studio->id) }}/edit">visibility</a>
+        <form method="POST" action="{{ url('admin/studios',$studio->id) }}" style="display: inline-block; margin: 0 0 0 5px">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button class="waves-effect waves-light btn red"><i class="material-icons right">delete</i>delete</button>
+        </form>
     </div>
     <div class="row">
-        <h3>Minibar content</h3>
-        <ul>
-            @foreach ($studio->minibarContents as $minibar)
-            <li>
-                <span>{{ $minibar->name }}</span>              
-            </li>
-            @endforeach
-        </ul>
-    </div>  --}}
+        <p>{{$studio->info}}</p>
+    </div>
 @endsection
 
 @section ('footer')
