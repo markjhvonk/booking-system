a9e2dc1a2c13c28b534d95bbffdfd4aad593195c
Mark Vonk <markjhvonk@gmail.com>
2017-10-10 13:37:04 +0200

    Added table for equipment relations

--- resources/views/admin/studios/studio.blade.php
+++ resources/views/admin/studios/studio.blade.php
@@ -26,9 +26,9 @@
         </div>
     </div>
     <div class="row">
-        <a class="waves-effect waves-light btn yellow darken-1" href="{{ url('admin/studios',$studio->id) }}/edit">edit</a>
-        <a class="waves-effect waves-light btn yellow darken-1" href="{{ url('admin/studios',$studio->id) }}/edit">visibility</a>
-        <form method="POST" action="{{ url('admin/studios',$studio->id) }}" style="display: inline-block; margin: 0 0 0 5px">
+        <a class="waves-effect waves-light btn yellow darken-1" href="{{ url('admin/studios',$studio->id) }}/edit"><i class="material-icons right">mode_edit</i>edit</a>
+        <a class="waves-effect waves-light btn green darken-1" href="{{ url('admin/studios',$studio->id) }}/edit"><i class="material-icons right">visibility</i>visibility</a>
+        <form method="POST" action="{{ url('admin/studios',$studio->id) }}" style="display: inline-block;">
             {{ method_field('DELETE') }}
             {{ csrf_field() }}
             <button class="waves-effect waves-light btn red"><i class="material-icons right">delete</i>delete</button>
@@ -36,6 +36,67 @@
     </div>
     <div class="row">
         <p>{{$studio->info}}</p>
+    </div>
+    <div class="row">
+        <h3>Available equipment</h3>
+    </div>
+    <div class="row">
+        <div class="col s6">
+            <h4>Equipment</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Data</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {{--  @foreach ($equipment as $equipment)  --}}
+                    <tr class="visibility">
+                        <td></td>
+                        <td class="description truncate"></td>
+                        <td>
+                            {{--  @if(!$equipment->data)
+                                N/A
+                            @else
+                                {{ $equipment->data }}
+                            @endif  --}}
+                        </td>
+                        <td>&pound;</td>
+                        <td></td>
+                        <td class="right-align">
+                            <a class="blue-text" href="/edit"><i class="material-icons">edit</i></a>
+                            <form method="POST" action="" style="display: inline-block;">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button class="red-text clear-btn" type="submit"><i class="material-icons">delete</i></button>
+                            </form>
+                            <form method="POST" action="/visible" style="display: inline-block;">
+                                {{ method_field('PATCH') }}
+                                {{ csrf_field() }}
+                                <button class="green-text clear-btn" type="submit">
+                                    <i class="material-icons">
+                                    {{--  @if($equipment->visible === 1)
+                                        visibility
+                                    @else
+                                        visibility_off
+                                    @endif  --}}
+                                    </i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                {{--  @endforeach  --}}
+                </tbody>
+            </table>
+        </div>
+        <div class="col s6">
+            <h4>Packages</h4>
+        </div>
     </div>
 @endsection
 
