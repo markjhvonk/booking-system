03566f0a85e7ec3fef48fcd3ffb3e0c12a81fcf9
Mark Vonk <markjhvonk@gmail.com>
2017-10-27 01:08:01 +0200

    Changed what users see when logged in as roles and when not

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,6 +2,12 @@
   <div class="nav-wrapper">
     <a href="#" class="brand-logo">Logo</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
+    @Auth
+      @if(Auth::user()->role == 3)
+      <li><a href="{{ url('/client') }}">Dashboard</a></li>
+      <li><a href="">Settings</a></li>
+      <li><a href="{{ url('/client/logout') }}">Logout</a></li>
+      @else
       <li><a href="{{ url('/admin') }}">Home</a></li>
       <li><a href="{{ url('/admin/studios') }}">Studios</a></li>
       <li><a href="{{ url('/admin/equipment') }}">Equipment</a></li>
@@ -14,6 +20,9 @@
         <a href="{{ url('admin/login') }}">Login</a> 
         @endauth
       </li>
+      @endif
+    @endauth
+      <li><a href="{{ url('/client/login') }}">Login</a></li>
     </ul>
   </div>
 </nav>
