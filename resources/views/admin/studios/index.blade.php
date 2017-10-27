243821b39cd2fbd6156e3c0caf635453b792207d
Mark Vonk <markjhvonk@gmail.com>
2017-10-28 00:03:15 +0200

    Changed studios index layput

--- resources/views/admin/studios/index.blade.php
+++ resources/views/admin/studios/index.blade.php
@@ -14,13 +14,29 @@
     @foreach ($studios as $studio)
 
         <div class="col s12 m4">
-            <div class="card medium blue-grey darken-1">
-                <div class="card-content white-text">
+            <div class="card medium">
+                <div class="card-image">
+                    @foreach ($studio->photo as $photo)
+                        @if($loop->first)
+                            <img src="{{asset('/storage/'.$photo->url)}}">
+                        @endif
+                    @endforeach
                     <span class="card-title">{{ $studio->name }}</span>
-                    <p>{{ $studio->info }}</p>
+                </div>
+                <div class="card-content">
+                    <p class="truncate">{{ $studio->info }}</p>
                 </div>
                 <div class="card-action">
-                    <a href="{{ url('admin/studios',$studio->id) }}">View</a>
+                    <a class="btn-flat left" href="{{ url('admin/studios',$studio->id) }}">View</a>
+                    <form method="POST" action="{{ url('admin/studios',$studio->id) }}/visible">
+                        {{ method_field('PATCH') }}
+                        {{ csrf_field() }}
+                        @if($studio->visible)
+                        <button type="submit" class="btn-flat right"><i class="material-icons green-text">visibility</i></button>
+                        @else
+                        <button type="submit" class="btn-flat right"><i class="material-icons gray-text">visibility_off</i></button>
+                        @endif
+                    </form>
                 </div>
             </div>
         </div>
