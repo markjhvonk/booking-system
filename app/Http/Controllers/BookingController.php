89ae3f5f24151e2275592d6394ff0bda5cb5f8fc
Mark Vonk <markjhvonk@gmail.com>
2017-10-27 22:02:25 +0200

    Created view and delete function

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    public function booking(Booking $booking)
+    {
+        return view('client.bookings.booking', compact('booking'));
+
+    }
+    public function delete(Request $request, Booking $booking)
+    {
+        $booking->delete();
+        return redirect()->route('clientDashboard');
+    }
 }
