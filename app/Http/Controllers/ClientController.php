063c1940a5b521c05630cf794988fc40c65fa5c9
Mark Vonk <markjhvonk@gmail.com>
2017-10-27 22:03:13 +0200

    Created edit function

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
@@ -9,5 +10,30 @@ class ClientController extends Controller
     public function index()
     {
         return view('client.index');
+    }
+
+    public function editAccount()
+    {
+        return view('client.profile.edit');
+    }
+    public function updateAccount(Request $request, User $user)
+    {
+        $this->validate(request(),[
+            'name'      => 'required',
+            'email'     => 'required|email',
+            'password'  => 'confirmed'
+            
+        ]);
+
+        if (empty($request->password)) {
+            $request = $request->except('password', 'password_confirmation');   // removes the password key from the request if password field is empty
+            $user->update($request);                                            // Updates the user with array
+        } else {
+            $user->update($request->all());                                     // Update user with form request
+            $user->password = bcrypt($request->password);                       // Hash new password
+            $user->save();
+        }
+
+        return back();
     }
 }
