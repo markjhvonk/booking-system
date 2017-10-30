a8f1012fc4682daff3f297dc4da50e85f043974a
Mark Vonk <markjhvonk@gmail.com>
2017-10-30 01:07:58 +0100

    Made email unique for new users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
         // Validate the form
         $this->validate(request(), [
             'name' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'password' => 'required|confirmed',
             'role' => 'required'
         ]);
